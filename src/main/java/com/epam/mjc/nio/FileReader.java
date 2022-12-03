82d7cd2a3d591f91860819f06a2032103a70423a
Igor Melnichenko <imelnicenko498@gmail.com>
2022-12-03 10:35:18 +0300

    Hm... fast result...

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,47 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
+    public String myReadLine (BufferedReader reader){
+        String result = "";
+        try {
+            result = reader.readLine();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return result;
+    }
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file){
+        Integer correctAge = 0;
+        Long correctNumber = 0L;
+        String[] nameLine = new String[2];
+        String[] emailLine = new String[2];
+        try (InputStream input = new FileInputStream(file)){
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            nameLine = myReadLine(reader).split(": ");
+            String[] ageLine = myReadLine(reader).split(": ");
+            emailLine = myReadLine(reader).split(": ");
+            String[] phoneLine = myReadLine(reader).split(": ");
+            try{
+                correctAge = Integer.valueOf(ageLine[1]);
+            }
+            catch (Exception e){
+                correctAge = null;
+            }
+            try {
+                correctNumber = Long.valueOf(phoneLine[1]);
+            }
+            catch (Exception e){
+                correctNumber = null;
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return new Profile(nameLine[1], correctAge, emailLine[1], correctNumber);
     }
 }
